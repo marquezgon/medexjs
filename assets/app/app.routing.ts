86c8760b743ae631351f62d55732ec836b7e02f2
Gonzalo Marquez <gmarquez@mexnetwork.com>
2016-08-23 22:00:13 -0500

    implement hash location strategy for routing

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,8 +2,7 @@ import {RouterModule, Routes} from "@angular/router";
 import {PacientesComponent} from "./pacientes/pacientes.component";
 
 const APP_ROUTES: Routes = [
-    { path: 'pacientes', component: PacientesComponent },
-    { path: '', redirectTo: '/pacientes', pathMatch: 'full'},
+    { path: 'pacientes', component: PacientesComponent }
 ];
 
-export const routing = RouterModule.forRoot(APP_ROUTES);
+export const routing = RouterModule.forRoot(APP_ROUTES, { useHash: true });
